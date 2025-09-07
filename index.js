71571e4be0468b1a899fa062a61bd367c93129b3
indra <138885587+idecratif@users.noreply.github.com>
2025-09-07 17:04:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+(() => {
+    Object.defineProperty(Array, 'empty', {
+      value: function empty(arg = []) {
+        if (typeof arguments[0] === 'undefined' || Array.isArray(arguments[0]) === false) return false;
+      
+        for (var i of arg) if (typeof i === 'string' && i.length === 0) return false;  
+        return arg.length > 0;
+      },
+      writable: false, // nggak bisa diubah
+      enumerable: false, // nggak nongol di loop
+      configurable: true,
+    });
+})();
